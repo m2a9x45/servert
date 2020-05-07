807e8619e3b4b245838005f7a511c1e343755e8e
m2a9x45 <35412216+m2a9x45@users.noreply.github.com>
2020-05-07 23:01:39 +0100

    Tasks order (#2)
    * Added task generation after order
    
    * Added task list in bizops
    
    * Hidden main bizops content until task is clicked
    
    * Added auth for staff
    
    Added time ago to tasks in bizops
    Added checks on client for staff login
    
    * Moved functions to correct location and changed token name for staff

---
+++ api/v1/api/routes/internal.go
@@ -0,0 +1,109 @@
+package routes
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"../database"
+	"../models"
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+func RefreshStaff(w http.ResponseWriter, r *http.Request) {
+	// (BEGIN) The code uptil this point is the same as the first part of the `Welcome` route
+	c, err := r.Cookie("token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	tknStr := c.Value
+	claims := &models.ClaimsStaff{}
+	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if !tkn.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	// (END) The code up-till this point is the same as the first part of the `Welcome` route
+
+	// We ensure that a new token is not issued until enough time has elapsed
+	// In this case, a new token will only be issued if the old token is within
+	// 30 seconds of expiry. Otherwise, return a bad request status
+	if time.Unix(claims.ExpiresAt, 0).Sub(time.Now()) > 75*time.Second {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Now, create a new token for the current use, with a renewed expiration time
+	expirationTime := time.Now().Add(5 * time.Minute)
+	claims.ExpiresAt = expirationTime.Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if claims.Role == "Staff" {
+		http.SetCookie(w, &http.Cookie{
+			Name:     "token",
+			Value:    tokenString,
+			Expires:  expirationTime,
+			HttpOnly: true,
+			Path:     "/",
+		})
+		return
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	// Set the new token as the users `token` cookie
+
+}
+
+func GetTasks(w http.ResponseWriter, r *http.Request) {
+
+	result, err := database.DBCon.Query("SELECT * from tasks")
+	if err != nil {
+		println(err)
+	}
+
+	var alltasks []models.Task
+
+	for result.Next() {
+		var task models.Task
+		err := result.Scan(&task.ID, &task.UUID, &task.UserID, &task.QueueID, &task.LinkID, &task.Assigned, &task.Status, &task.CreatedAt)
+		if err != nil {
+			panic(err.Error())
+		}
+		alltasks = append(alltasks, task)
+	}
+
+	println(len(alltasks))
+
+	if len(alltasks) == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		res := models.ResObj{Success: false, Message: "No tasks"}
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+
+	fmt.Println(alltasks)
+	json.NewEncoder(w).Encode(alltasks)
+}
---
+++ web/bizops/main.js
@@ -0,0 +1,206 @@
+// make call to get tasks.
+const tasks = document.querySelectorAll(".task");
+const tasksList = document.querySelector(".tasksList");
+const content = document.querySelector(".content");
+const serverTask = document.querySelector(".serverTask")
+const loginFrom = document.querySelector(".loginFrom");
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+const contaniner = document.querySelector(".contaniner");
+const URL_API = "http://127.0.0.1:8000";
+
+window.addEventListener("load", () => {
+    fetch(`${URL_API}/auth/isstaffloggedin`, {
+        method: 'get',
+        credentials: 'include',
+        headers: {
+            "Content-type": "application/json",
+        }
+    })
+    .then(response => {
+        if (response.status == 401) {
+            console.log(response.status);
+            return {"success" : false};
+        } else {
+            return response.json();
+        }    
+    })
+    .then (data => {
+        console.log(data);
+
+        if (data.success == true) {
+            loginFrom.style.display = "none";
+            contaniner.style.display = "grid";
+            getTasks();
+        }
+
+    })
+    .catch(function (error) {
+        console.log('Request failed', error);
+    });
+});
+
+loginFrom.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let formData = {
+        "email" : username.value,
+        "password" : password.value,
+    }
+
+    fetch(`${URL_API}/auth/stafflogin`, {
+        method: 'post',
+        credentials: 'include',
+        headers: {
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then((data) => {
+        console.log(data);
+
+        if (data.success == true) {
+            loginFrom.style.display = "none";
+            contaniner.style.display = "grid";
+            getTasks();
+        }
+
+    })
+    .catch(function (error) {
+        console.log('Request failed', error);
+    });
+    
+})
+
+function getTasks() {
+    fetch(`${URL_API}/internal/tasks`, {
+        method: 'get',
+        credentials: 'include',
+        headers: {
+            "Content-type": "application/json",
+        }
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        addTaskToPage(data);
+    })
+    .catch(err => console.log(err))
+}
+
+function addTaskToPage(tasks) {
+
+    tasks.forEach(task => {
+        
+        let divTask = document.createElement("div");
+        divTask.setAttribute("class", "task");
+        divTask.setAttribute("id", task.LinkID);
+
+        let divTitle = document.createElement("div");
+        divTitle.setAttribute("class", "taskTitle");
+
+        let name = document.createElement("p");
+        name.innerText = task.ID;
+
+        let made = document.createElement("p");
+
+        let now = new Date().getTime();
+        let created = new Date(task.CreatedAt * 1000).getTime();
+
+        // console.log(now);
+        // console.log(created);
+
+        let diff = now - created;
+        let secs = diff / 1000;
+        let mins = secs / 60;
+        let hours = mins / 60;
+        let days = hours / 24;
+
+        if (secs <= 60) {
+            // display in seconds as it's less than an minits
+            // console.log("seconds :", Math.round(secs));
+            made.innerText = `${Math.round(secs)} Seconds ago`
+        } else if (mins <= 60) {
+            // display in minitues as it's less than an hour
+            //console.log("minitues :", Math.round(mins));      
+            made.innerText = `${Math.round(mins)} minutes ago`
+        } else if (hours <= 24) {
+            // display in hours as it's less than an days
+            //console.log("hours :", Math.round(hours));     
+            made.innerText = `${Math.round(hours)} hours ago`
+        } else {
+            //console.log("days :", Math.round(days));
+            made.innerText = `${Math.round(days)} days ago`
+            
+        }
+
+        let status = document.createElement("p");
+        status.innerText = task.Status;
+
+        divTask.addEventListener("click", (e) => {
+            serverTask.innerHTML = "";
+            selectTask(e, task.UUID, task.LinkID, task.UserID);
+        });
+
+        divTitle.appendChild(name);
+        divTitle.appendChild(made);
+
+        divTask.appendChild(divTitle);
+        divTask.appendChild(status);
+
+        tasksList.appendChild(divTask);
+
+
+
+    });
+
+}
+
+window.onpopstate = function(event) {
+    alert("location: " + document.location + ", state: " + JSON.stringify(event.state));
+  };
+
+function selectTask(e, taskID, LinkID, User_ID) {
+    // console.log(taskID, LinkID, User_ID);
+    let url = window.history.pushState( {} , '', `?task_id=${taskID}` );
+    content.style.visibility = "visible";
+
+    let OrderID = document.createElement("p");
+    OrderID.innerText = LinkID;
+
+    let UserID = document.createElement("p");
+    UserID.innerText = User_ID;
+
+    let closeTask = document.createElement("button");
+    closeTask.innerText = "Close Task";
+    closeTask.addEventListener("click", () => {
+        console.log(taskID, "closed");
+        
+    });
+
+    serverTask.appendChild(OrderID);
+    serverTask.appendChild(UserID);
+    serverTask.appendChild(closeTask);
+
+}
+
+setInterval(function() {
+    refershToken();
+}, 60000); // Every 1 minitue
+
+
+function refershToken() {
+    fetch(`${URL_API}/internal/refresh`, {
+        method: 'get',
+        credentials: 'include',
+        headers: {
+            "Content-type": "application/json",
+        }
+    })
+    .then(response => response.json())
+    .then (data => console.log(data))
+    .catch(function (error) {
+        console.log('Request failed', error);
+    });
+}
