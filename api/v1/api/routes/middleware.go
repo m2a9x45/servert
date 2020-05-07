95e4dc08af4d2aec5104b396f5798e4eb3d2ab41
m2a9x45 <35412216+m2a9x45@users.noreply.github.com>
2020-05-07 23:09:08 +0100

    Tasks order (#4)
    * Added task generation after order
    
    * Added task list in bizops
    
    * Hidden main bizops content until task is clicked
    
    * Added auth for staff
    
    Added time ago to tasks in bizops
    Added checks on client for staff login
    
    * Moved functions to correct location and changed token name for staff

---
+++ api/v1/api/routes/middleware.go
@@ -0,0 +1,58 @@
+package routes
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"../models"
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+func Staffauth(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("middleware", r.URL)
+
+		c, err := r.Cookie("staff_token")
+		if err != nil {
+			if err == http.ErrNoCookie {
+				// If the cookie is not set, return an unauthorized status
+				fmt.Println(err)
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			// For any other type of error, return a bad request status
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		tknStr := c.Value
+
+		claims := &models.ClaimsStaff{}
+
+		tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+
+		if err != nil {
+			if err == jwt.ErrSignatureInvalid {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if !tkn.Valid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		if claims.Role != "Staff" {
+			fmt.Println("Token doesn't have the right role")
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
+}
